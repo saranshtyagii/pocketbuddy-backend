5dbbfa8cea933cc3994d3e714f46f8694102fe79
Saransh Tyagi <saransh@Saranshs-MacBook-Air.local>
2025-02-08 10:38:15 +0530

    initial commit

---
+++ src/main/java/com/web_pocketbuddy/Pocket/Buddy/PocketBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.web_pocketbuddy.Pocket.Buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_pocketbuddy/Pocket/Buddy/PocketBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_pocketbuddy.Pocket.Buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
