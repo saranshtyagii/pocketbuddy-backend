f4f0ee7d8dbd1d752ffb43680a728c37128ddc34
Saransh Tyagi <saransh@Saranshs-MacBook-Air.local>
2025-04-08 11:01:40 +0530

    implement redis

--- src/main/java/com/web/pocketbuddy/PocketBuddyApplication.java
+++ src/main/java/com/web/pocketbuddy/PocketBuddyApplication.java
@@ -1,12 +1,28 @@
 package com.web.pocketbuddy;
 
+import com.web.pocketbuddy.entity.helper.GenerateUtils;
+import com.web.pocketbuddy.entity.helper.RedisUtils;
+import com.web.pocketbuddy.service.response.RedisServices;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+@AllArgsConstructor
 public class PocketBuddyApplication {
+
+	private final RedisUtils redisUtils;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocketBuddyApplication.class, args);
 	}
+
+	@PostConstruct
+	public void init() {
+		GenerateUtils.initialize(redisUtils);
+	}
+
 }
