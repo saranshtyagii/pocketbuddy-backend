c9954a75ada26b6cb9e25dc830ecb997fafa0bf7
Saransh Tyagi <saransh@Saranshs-MacBook-Air.local>
2025-04-18 10:36:39 +0530

    cross config

--- src/main/java/com/web/pocketbuddy/config/ApplicationConfiguration.java
+++ src/main/java/com/web/pocketbuddy/config/ApplicationConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 
@@ -20,6 +22,19 @@ public class ApplicationConfiguration {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // allow all endpoints
+                        .allowedOrigins("*") // allow all origins (or specify frontend origin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 
